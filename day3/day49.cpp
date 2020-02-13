8df5fd04a343f213b945edc3d8350a17bf1c0560
tyttyy shanee <shaneetyttyy@qq.com>
2020-02-13 10:51:05 +0800

    D:\shi\buptpro\doc_trail\leetcodeEveryday\day3\day3\day50.cpp Add

--- day3/day49.cpp
+++ day3/day49.cpp
@@ -3,6 +3,17 @@ using namespace std;
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-
+        unordered_map<string, vector<string>> mmp;
+        vector<vector<string>> result;
+        string tmp;
+        for (string& str : strs) {
+            tmp = str;
+            sort(tmp.begin(), tmp.end());
+            mmp[tmp].push_back(str);
+        }
+        for (auto cstrs : mmp) {
+            result.push_back(cstrs.second);
+        }
+        return result;
     }
 };
